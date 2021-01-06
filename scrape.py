7a09964da5d72bf54a1955bfa87adefaa43df5c5
Ioan Zicu <ioan.zicu94@gmail.com>
2021-01-06 04:48:14 +0100

    sort and scrape multiple pages

--- scrape.py
+++ scrape.py
@@ -1,11 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 import pprint
+from time import sleep
+from random import randint
 
-res = requests.get('https://news.ycombinator.com/news')
-soup = BeautifulSoup(res.text, 'html.parser')
-links = soup.select('.storylink')
-subtext = soup.select('.subtext')
+
+def sort_stories_by_votes(hnlist):
+    return sorted(hnlist, key=lambda k: k['votes'], reverse=True)
 
 
 def create_custom_hn(links, subtext):
@@ -19,7 +20,25 @@ def create_custom_hn(links, subtext):
             if points > 99:
                 hn.append({'title': title, 'link': href, 'votes': points})
 
-    return hn
+    return sort_stories_by_votes(hn)
 
 
-pprint.pprint(create_custom_hn(links, subtext))
+hn_links = []
+hn_subtext = []
+
+# Scrape first 2 pages
+for index in range(1, 3):
+    URL = 'https://news.ycombinator.com/news?p=' + str(index)
+    res = requests.get(URL)
+    soup = BeautifulSoup(res.text, 'html.parser')
+    links = soup.select('.storylink')
+    subtext = soup.select('.subtext')
+
+    hn_links.extend(links)
+    hn_subtext.extend(subtext)
+
+    # Time interval between requests
+    sleep(randint(2, 5))
+
+
+pprint.pprint(create_custom_hn(hn_links, hn_subtext))
